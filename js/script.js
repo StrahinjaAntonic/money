30b939ae026a78b4ad86cc2ab9730c9c6ad4bdc9
Bogdan <bogdanmaksimovic91@gmail.com>
2019-01-10 16:18:28 +0100

    testtt

--- js/script.js
+++ js/script.js
@@ -1,100 +1,57 @@
 $(document).ready(function() {
 
-    const btnSubmit = $('#submit');
-    const btnStorage = $('#storage');
-    const btnClear = $('#clear');
-    const getDate = $('.month');
-    const table = $('.table');
+    var now, months, month, year;
+    
+    now = new Date();
+    
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    month = now.getMonth();
+    
+    year = now.getFullYear();
 
-    (function() {
-        var now, months, month, year;
+    $('.month').text(`${months[month]} ${year}`);
+    
+		$('#submit').click(function(){
+			let date = $('#date').val();
+			let cat = $('#categories option:selected').val()
+			let number = $('#number').val();
+			let message = $('#message').val();
+			console.log(date, cat,number,message);
 
-        now = new Date();
+			var table = $('tbody').append(`
+						<tr>
+		                    <td>${date}</td>
+		                    <td>${cat}</td>
+		                    <td>${number}</td>
+		                    <td>${message}</td>
+		                </tr>`);
 
-        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        month = now.getMonth();
+			$('.table').DataTable();
 
-        year = now.getFullYear();
+			localStorage.setItem("date", date);
+			localStorage.setItem("cat", cat);
+			localStorage.setItem("num", number);
+			localStorage.setItem("message", message);
+			
+	   	})
 
-        getDate.text(`${months[month]} ${year}`);
-    })();
+	   	/*$('#clear').click(function(){
+	   		localStorage.clear();
+	   	})*/
+    
+	   	var date = localStorage.getItem("date");
+	   	var cat = localStorage.getItem("cat");
+	   	var number = localStorage.getItem("num");
+	   	var message = localStorage.getItem("message");
 
-    btnSubmit.click(function() {
-        let currDate = $('#date').val();
-        let categories = $('#categories option:selected').val()
-        let number = $('#number').val();
-        let message = $('#message').val();
+	   	var table = $('tbody').append(`
+						<tr>
+		                    <td>${date}</td>
+		                    <td>${cat}</td>
+		                    <td>${number}</td>
+		                    <td>${message}</td>
+		                </tr>`);
 
-        var string = `{"items":[
-				{"currentDate":${currDate}},
-				{"categories":${categories}},
-				{"number":${number}},
-				{"message":${message}}
-			]}`;
-        var json = { "items": [{ "currentDate": currDate }, { "categories": categories }, { "number": number }, { "message": message }] };
-
-        localStorage.setItem(`added-items`, AddToLocalStorage(string));
-        localStorage.setItem(`added-items`, AddToLocalStorage(json));
-
-        function AddToLocalStorage(data) {
-            if (typeof data != "string") { data = JSON.stringify(data); }
-            return data;
-        }
-
-        // this function gets string from localStorage and converts it into JSON
-
-        // check this//
-        function GetFromLocalStorage(key) {
-            return JSON.parse(localStorage.getItem(key));
-        }
-
-        var myData = GetFromLocalStorage(`added-items`);
-
-        //console.log(myData);
-
-        var currentDate = myData.items[0].currentDate;
-        var currCat = myData.items[1].categories;
-        var num = myData.items[2].number;
-        var msg = myData.items[3].message;
-
-        function fillTable() {
-            table.append(`<tbody>`).append(`
-							<tr>
-			                    <td>${currentDate}</td>
-			                    <td>${currCat}</td>
-			                    <td>${num}</td>
-			                    <td>${msg}</td>
-			                </tr>`);
-            table.DataTable()
-        }
-        fillTable();
-    });
-
-    btnStorage.one('click', function(e) {
-        function GetFromLocalStorage(key) {
-            var dateFromLocalStorage = JSON.parse(localStorage.getItem(key));
-            //return JSON.parse(localStorage.getItem(key));
-            let currDate = dateFromLocalStorage.items[0].currentDate
-            let currCat = dateFromLocalStorage.items[1].categories;
-            let num = dateFromLocalStorage.items[2].number;
-            let msg = dateFromLocalStorage.items[3].message;
-
-            $('.table').append(`<tbody>`).append(`
-								<tr>
-				                    <td>${currDate}</td>
-				                    <td>${currCat}</td>
-				                    <td>${num}</td>
-				                    <td>${msg}</td>
-				                </tr>`);
-            table.DataTable()
-        }
-        GetFromLocalStorage(`added-items`);
-    });
-
-    btnClear.one('click', function() {
-        (function() {
-            localStorage.clear();
-            location.reload();
-        })();
-    });
+			$('.table').DataTable();
+    
 });
